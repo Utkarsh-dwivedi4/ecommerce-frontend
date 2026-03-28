003f0c4260626a30f7f2543f19b20baed02b299d
Utkarsh-dwivedi4 <your-github-email>
2026-03-28 12:44:20 +0530

    first commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ecommerce-backend-production-d64c.up.railway.app/api"
+  baseURL: "https://ecommerce-backend-production-d64c.up.https://railway.com/project/03ecd446-d856-4302-ad8a-101c7326dca7/service/40b45ca5-9cc8-4f59-ba11-ac08962d49e2?environmentId=b07907ec-052d-4f95-95f4-81839fc5471d&id=c0443d8c-9576-4956-9ce0-576e4046ba2b#deploy"
 });
 
 export default API;
